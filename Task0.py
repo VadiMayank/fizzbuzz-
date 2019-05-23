16e4de628e3d15f018e08cecee53df1ff88e9a1d
VadiMayank <Gmayankvadi@dal.ca>
2019-05-23 05:40:00 -0700

    Printing 1 to 100 with multiples of 3 as fizz and 5 as buzz

--- Task0.py
+++ Task0.py
@@ -1,10 +1,9 @@
-i=0
-while i<101:
-		if i%3==0:
-			print("Fizz")
-		elif i%5==0:
-			print("Buzz")
-		elif i%3==0 and i%5==0:
-			print("FizzBuzz")
-		print(i)
-		i+=1
+def fizzbuzz(max_num=101):
+    for i in range(max_num):
+        value = ""
+        if i % 3 == 0: value += "Fizz"
+        if i % 5 == 0: value += "Buzz"
+        yield value if value else i
+
+for number, burp in enumerate(fizzbuzz()):
+print "%s: %s" % (number, burp)
